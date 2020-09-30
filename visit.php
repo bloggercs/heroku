c20d84a2040756bffe74700a21abb2e691ce0930
bloggercs <54446304+bloggercs@users.noreply.github.com>
2020-09-30 21:26:53 +0300

    Add files via upload

---
+++ visit.php
@@ -0,0 +1,42 @@
+<?php 
+ignore_user_abort(true);
+error_reporting(0);
+set_time_limit(0);
+
+function file_getcontent_with_proxy($urltoget) {
+    $url = $urltoget;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // read more about HTTPS 
+    curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Linux; Android 10; IN2011 Build/QKQ1.191222.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/84.0.4147.89 Mobile Safari/537.36 GSA/11.20.15.21.arm64');
+    $curl_scraped_page = curl_exec($ch);
+    curl_close($ch);
+    return $curl_scraped_page;
+}
+
+
+$urls = array (
+  array("test","https://cron-job.org"),
+  array("test2","https://cron-job.org"),
+);
+$st = count($urls);
+//echo "Start ht";
+while ( 1 ){
+    $dt = 0;
+    foreach($urls as $key=>$val){
+        $name = $val[0];
+        $url = $val[1];
+        $res = file_getcontent_with_proxy($url);
+        if($res){
+        $dt += 1;
+        }
+    }
+    //echo "sleeping<br>";
+    file_put_contents("php://stderr", "Tot:$st - Succes:$dt\n");
+    sleep(60);
+}
+?>
