34329d5138e76b37ff63682f02bb62062a9603cc
bloggercs <54446304+bloggercs@users.noreply.github.com>
2020-10-07 23:08:56 +0300

    Update visit.php

--- visit.php
+++ visit.php
@@ -65,6 +65,7 @@ $urls = array (
   array("suzanne27","http://kandillihamit.pserver.ru/suzanne27/index.php"),
   array("suzanne27","http://kandillihamit.pserver.ru/suzanne27/index.php"),
   array("xansolo","http://kandillihamit.pserver.ru/xansolo/index.php"),
+  array("jacmieewegerr","http://kandillihamit.pserver.ru/jacmieewegerr/index.php"),
 );
 $st = count($urls);
 //echo "Start ht";
